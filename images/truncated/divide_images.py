f5027c2763687f4408883328cdf9e2ce6a396da6
chrispy67 <109503264+chrispy67@users.noreply.github.com>
2023-05-24 23:14:02 -0700

    added divided up images for everybody in directories with their name

---
+++ images/truncated/divide_images.py
@@ -0,0 +1,18 @@
+import glob as glob
+import sys
+import shutil
+
+pwd = '{}/'.format(sys.path[0])
+
+images = [name.replace(pwd, '') for name in glob.glob(pwd+'*.png')]
+print(len(images))
+
+members = ['Mark', 'Christian', 'Jon', 'Chris', 'Quentin']
+divvy = len(images)/ len(members)
+print(divvy)
+
+for member in members:
+    dir = pwd + '{}/'.format(member)
+    print("moving truncated images to {}'s directory".format(member))
+    for i in range(int(divvy)):
+        shutil.copy(pwd+images[i], dir)
